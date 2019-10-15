4c0c27541c9e15ab8c50e8b33c87819eb23f66fd
Tushar Velingkar <tushar.velingkar@creativecapsule.com>
2019-10-15 12:36:24 +0530

    Pure Component and Hooks usage
    * Converted class component AddTodo to pure component.
    * Implemented useState hook in AddTodo to showcase usage of hooks.

--- src/components/TodoPage/AddTodo/AddTodo.js
+++ src/components/TodoPage/AddTodo/AddTodo.js
@@ -1,65 +1,51 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import {addTodo} from "../actions/todos";
 
-class AddTodo extends Component {
-    initialState = {
-        title: '',
-        isComplete: false
-    }
-    constructor(props) {
-        super(props);
-        this.state = this.initialState;
+const AddTodo = ({addTodo}) => {
+    const [title, setTitle] = useState('');
+    const [isComplete, setIsComplete] = useState(false);
 
-        this.handleSubmit = this
-            .handleSubmit
-            .bind(this);
-        this.handleTitleChange = this
-            .handleTitleChange
-            .bind(this);
-    }
-
-    static whyDidYouRender = true
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} className="d-flex p-2 bd-highlight">
-                <div className="input-group mb-3">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Task Title"
-                        aria-label="Task Title"
-                        aria-describedby="button-submit"
-                        value={this.state.title}
-                        onChange={this.handleTitleChange}/>
-                    <div className="input-group-append">
-                        <button className="btn btn-primary" type="submit" id="button-submit">Add Task</button>
-                    </div>
-                </div>
-            </form>
-        );
-    }
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        if (this.state.isComplete) {
+        if (isComplete) {
             let todo = {
-                title: this.state.title
+                title: title
             };
-            this.setState(this.initialState)
-            this
-                .props
-                .addTodo(todo);
+            setTitle('');
+            setIsComplete(false);
+            addTodo(todo);
         }
     }
 
-    handleTitleChange(event) {
-        this.setState({title: event.target.value, isComplete: true});
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+        setIsComplete(true);
     }
+
+    return (
+        <form onSubmit={handleSubmit} className="d-flex p-2 bd-highlight">
+            <div className="input-group mb-3">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Task Title"
+                    aria-label="Task Title"
+                    aria-describedby="button-submit"
+                    value={title}
+                    onChange={handleTitleChange} />
+                <div className="input-group-append">
+                    <button className="btn btn-primary" type="submit" id="button-submit">Add Task</button>
+                </div>
+            </div>
+        </form>
+    );
 }
 
 const mapDispatchToProps = {
     addTodo
 };
+
+AddTodo.whyDidYouRender = true;
 
 export default connect(null, mapDispatchToProps)(AddTodo);
